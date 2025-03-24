6684b6d209bb8fd556fbca8ae1248b7d6f4e6165
s-A121-18 <s-A121-18@CAMPUS.UEK.KRAKOW.PL>
2025-03-24 16:01:02 +0100

    Leaflet map fixed

--- app.js
+++ app.js
@@ -29,23 +29,31 @@ function showPage(page) {
 }
 
 function getLocation() {
+  showPage("mapid")
+  
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
       let lat = position.coords.latitude;
       let lon = position.coords.longitude;
       let map = L.map("mapid").setView([lat, lon], 13);
-      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(
-        map
-      );
+      // L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
       L.marker([lat, lon])
         .addTo(map)
         .bindPopup("Twoja lokalizacja")
         .openPopup();
+      setTimeout(() => { 
+        map.invalidateSize(); 
+      }, 500);
     });
   } else {
     alert("Twoja przeglądarka nie obsługuje geolokalizacji.");
   }
 }
+
 
 function sendSOS() {
   navigator.vibrate([300, 300, 300, 900, 900, 900, 300, 300, 300]);
